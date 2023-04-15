8bddd6c3c047a5e92bb02ebf97bfca27304e4192
tkulagina <tkulagina1974@gmail.com>
2023-04-15 12:16:00 +0300

    task 2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+console.log(SimpleLightbox);
+
+const listRef = document.querySelector('.gallery');
+const markup = galleryItems.map(({preview, original, description}) => `<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+   <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`).join('');
+
+listRef.insertAdjacentHTML('beforeend', markup)
+
+
+new SimpleLightbox('.gallery__item a', {
+    captions: true,
+    captionType: 'attr', 
+    captionsData: `alt`,
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
